eff912ddec9c22c0504d372beb353c9922a0db98
Mangesh Pawar <mangeshpawarmrp2004@gmail.com>
2025-04-06 03:35:38 +0530

    letter-case issue solve

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,7 +5,7 @@ const Signup = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
       <div className="w-full max-w-md bg-white p-6 rounded-2xl shadow-xl">
         <h2 className="text-2xl font-bold text-center mb-4">
-          Join BeejSeBazaar 🌱
+          Join BeejSeBazaar now 🌱
         </h2>
         <SignUp path="/signup" routing="path" afterSignUpUrl="/dashboard" />
       </div>
